08194b4303b47f2709b9a2f3027ca501be50d92a
Anvesh D <danvesh1992@gmail.com>
2021-02-16 11:44:12 -0600

    Build a Customer Notification Service commit

--- src/main/kotlin/com/kotlin/orderapp/CartService.kt
+++ src/main/kotlin/com/kotlin/orderapp/CartService.kt
@@ -1,5 +1,15 @@
 package com.kotlin.orderapp
 
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.KafkaProducer
+import java.util.Properties
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.time.Duration
+
 class CartService {
 
 	fun submitUserOrder(args: Array<String>): Double {
@@ -52,5 +62,38 @@ class CartService {
 		println("total amount for the selected inputs with Offer is : " + (amount / 100))
 
 		return (amount / 100)
+	}
+
+	fun publishOrderEvent() {
+		val kafkaproducer: Producer<String, String> = createKafkaOrderProducer("localhost:9092")
+		kafkaproducer.send(ProducerRecord("cart-order-submit", "Order is submitted!!")).get()
+	}
+
+	fun createKafkaOrderProducer(brokers: String): Producer<String, String> {
+		val properties = Properties()
+		properties["bootstrap.servers"] = brokers
+		properties["key.serializer"] = StringSerializer::class.java.canonicalName
+		properties["value.serializer"] = StringSerializer::class.java.canonicalName
+		return KafkaProducer<String, String>(properties)
+	}
+
+	fun consumeOrderEvent() {
+		val kafkaConsumer: Consumer<String, String> = createKafkaOrderConsumer("localhost:9092")
+		kafkaConsumer.subscribe(listOf("cart-order-submit"));
+		while (true) {
+			val cartOrderRecords = kafkaConsumer.poll(Duration.ofSeconds(1))
+			println(cartOrderRecords);
+		}
+
+	}
+
+
+	fun createKafkaOrderConsumer(brokers: String): Consumer<String, String> {
+		val properties = Properties()
+		properties["bootstrap.servers"] = brokers
+		properties["group.id"] = "person-processor"
+		properties["key.deserializer"] = StringDeserializer::class.java
+		properties["value.deserializer"] = StringDeserializer::class.java
+		return KafkaConsumer<String, String>(properties)
 	}
 }
